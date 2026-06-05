f59753198e417ad8cacd0365d96989ba2c9d8c11
Daniel O'Malley <danielomalley@redcross.org.uk>
2026-06-05 14:15:44 +0100

    test: barebones

--- app/[lang]/[[...slug]]/page.tsx
+++ app/[lang]/[[...slug]]/page.tsx
@@ -3,6 +3,10 @@ interface GeneratedPath {
 	slug: string[];
 }
 
+export const dynamicParams = true;
+
+export const dynamic = "force-static";
+
 export async function generateStaticParams(): Promise<GeneratedPath[]> {
 
 	// Add locale URLs to the generated paths
@@ -22,6 +26,8 @@ export default async function Page(props: {
 	params: Promise<GeneratedPath>;
 }): Promise<React.JSX.Element> {
     const params = await props.params;
+
+    console.log('Received params:', params); // Log the received parameters for debugging
 	const { slug, lang } = params;
 
     // Timestamp when the page is generated
@@ -31,7 +37,7 @@ export default async function Page(props: {
     return (<div>
         <h1>Generated Page</h1>
         <p>Language: {lang || 'default'}</p>
-        <p>Slug: {slug.join('/') || 'home'}</p>
+        <p>Slug: {slug?.join('/') || 'home'}</p>
         <p>Page generated at: {generationTime}</p>
     </div>);
 }
